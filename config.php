27777d29c3c78c89922cf2a390ff11cd1048379d
Prince Costa <princecosta9@gmali.com>
2023-05-07 08:32:11 +0600

    php conversion

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// $DocRoot = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR;
+$DocRoot = $_SERVER['DOCUMENT_ROOT'].'/campus_canteen';
+
+$head = $DocRoot.'/partials/head.php';
+$topNav = $DocRoot.'/partials/top_nav.php';
+$useroparetionsAndAvatar = $DocRoot.'/partials/user_oparetions_and_avatar.php';
+$mainSideNav = $DocRoot.'/partials/main_side_nav.php';
+$pageHeaderNav = $DocRoot.'/partials/page_header_nav.php';
+$footer = $DocRoot.'/partials/footer.php';
+?>
