69b096ea967e837e5ce261b77c7be7bd21e29e5b
saroaremon <saroar01834@gmail.com>
2023-05-15 12:56:30 +0600

    Frontend Slider Added

--- config.php
+++ config.php
@@ -14,4 +14,5 @@ $webroot = "http://campus_canteen.test".DIRECTORY_SEPARATOR;
 $docroot = $_SERVER['DOCUMENT_ROOT'];
 $partialAdmin = $docroot.DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'partials'.DIRECTORY_SEPARATOR;
 $partialFrontend = $docroot.DIRECTORY_SEPARATOR.'frontend'.DIRECTORY_SEPARATOR.'partials'.DIRECTORY_SEPARATOR;
-$adminResources = $docroot.DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR;
+$adminResources = $docroot.DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'data_source'.DIRECTORY_SEPARATOR;
+$frontendResources = $docroot.DIRECTORY_SEPARATOR.'frontend'.DIRECTORY_SEPARATOR.'data_source'.DIRECTORY_SEPARATOR;
