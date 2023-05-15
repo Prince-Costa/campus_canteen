baea76ef29a9ae82855371cc118320c3388c1885
Prince Costa <princecosta9@gmali.com>
2023-05-15 08:27:07 +0600

    adding json formet for nav

--- config.php
+++ config.php
@@ -14,3 +14,4 @@ $webroot = "http://campus_canteen.test".DIRECTORY_SEPARATOR;
 $docroot = $_SERVER['DOCUMENT_ROOT'];
 $partialAdmin = $docroot.DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'partials'.DIRECTORY_SEPARATOR;
 $partialFrontend = $docroot.DIRECTORY_SEPARATOR.'frontend'.DIRECTORY_SEPARATOR.'partials'.DIRECTORY_SEPARATOR;
+$adminResources = $docroot.DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR;
